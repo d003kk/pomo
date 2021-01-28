9af7f4b79f753b8b38dacbedc4c6262dddfd0436
Ben Sims <benjamin.james.sims@gmail.com>
2021-01-28 21:29:34 +0000

    Added loop for timer

--- timer.cpp
+++ timer.cpp
@@ -13,17 +13,42 @@ using namespace std;
 template< class Clock >
 class timer
 {
-  typename Clock::time_point start;
 public:
-  timer() : start( Clock::now() ) {}
+  timer()
+  : start( Clock::now() )
+  , stopped(false)
+  {}
   typename Clock::duration elapsed() const
   {
     return Clock::now() - start;
   }
-  long int seconds() const
+  long int display() const
   {
     return round<boost::chrono::seconds>(elapsed() ).count();
   }
+  void  reset()
+  {
+  	start = Clock::now();
+  }
+  void  stop()
+  {
+  	stopped = true; 
+  }
+  void  restart()
+  {
+  	stopped = false; 
+  }
+private:
+  bool stopped;
+  typename Clock::time_point start;
+  typename Clock::time_point stopped_time;
+};
+
+enum{
+	STOP=0,
+	DISPLAY,
+	RESTART,
+	RESET
 };
 
 
@@ -31,10 +56,25 @@ void do_stuff()
 {
 
  timer<system_clock> t1;	
+ int option = 0;
  while(1) {
 
-	 cout<<t1.seconds()<<endl;
-	 
+	 cout<<"Options Stop, Display, Restart, Reset"<<endl;
+	 cin>>option;
+	 switch(option) {
+		 case STOP:
+			 t1.stop();
+			 break;
+		 case DISPLAY:
+	 		cout<<t1.display()<<endl;
+			break;
+		 case RESTART:
+			t1.restart();
+			break;
+		 case RESET:
+			t1.reset();
+			break;
+	 };
  }
 
 
